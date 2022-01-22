531bc2a4fe1997be433be7dfedf8f40360895d33
henter <1440159612@qq.com>
2022-01-22 10:26:34 +0800

    first commit

---
+++ gitTest01/src/main/java/com/henter/Test01.java
@@ -0,0 +1,11 @@
+package com.henter;
+
+/**
+ * @author henter
+ * @create 2022-01-22 10:09
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("i am henter");
+    }
+}
---
+++ gitTest01/src/main/java/com/henter/bean/Student.java
@@ -0,0 +1,8 @@
+package com.henter.bean;
+
+/**
+ * @author henter
+ * @create 2022-01-22 10:15
+ */
+public class Student {
+}
